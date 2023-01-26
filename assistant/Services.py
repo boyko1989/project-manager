72448d55e74ac9479a905343acd03c9fa83ac152
boyko1989 <hunwiss89@gmail.com>
2023-01-26 15:56:41 +0400

    Три способа записи в БД

--- assistant/Services.py
+++ assistant/Services.py
@@ -2,6 +2,9 @@ from Models import *
 
 # Core Functionality
 with db:
-    db.create_tables([ThemeModel, ProjectModel])
+    # db.create_tables([ThemeModel, ProjectModel])
+    python = ThemeModel(name='Python', path='/home/p_boyko/Projects/PYTHON').save()
+    bash = ThemeModel.create(name='Bash', path='/home/p_boyko/Projects/BASH')
+    network = ThemeModel.insert(name='Network', path='/home/p_boyko/Projects/NETWORK').execute()
 
 print('Done')
