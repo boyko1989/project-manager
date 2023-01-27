817a704b9712bc87e045e2d8a5eb67b1b21f43d1
boyko1989 <hunwiss89@gmail.com>
2023-01-27 17:14:04 +0400

    Добавил чтение параметров (argparse)

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
 from assistant.Indexator import Indexator as path_handler
 from assistant.Viewer import Viewer as view
+import argparse
 
 # import expir
 
-if __name__ == '__main__':
-    """Просматриваем все каталоги и подкаталоги от этого уровня и ниже
-    
-    1. Определяем структуру:
-        1. Создаём дерево тем
-        2. В качестве листьев предполагаем отдельные проекты
-        3. Выделяем документацию (те подкаталоги, в которых количество .md файлов > 1)
-        4. Исключаем из анализа данный проект
-    
-    """
+parser = argparse.ArgumentParser(
+    prog='Project Manager',
+    description='Эта программа помогает отслеживать содержимое каталога с проектами'
+)
+parser.add_argument('-g', '--get', type=str, default='')
+parser.add_argument('-l', '--lst', type=str, default='')
 
-    th_kwargs = {
+args = parser.parse_args()
+
+
+def get(arg):
+    kwargs = {
         'type_fild': 'type',
-        'type_of_search': 'theme',
+        'type_of_search': args.get,
     }
 
-    th = path_handler()
-    th_view = view()
-    dict_of_themes = th.dir_travel(th.fild_search, th_kwargs)
-    th_view.sorter(dict_of_themes)
+    ess = path_handler()
+    ess_view = view()
+    dict_of_essence = ess.dir_travel(ess.fild_search, kwargs)
+    ess_view.sorter(dict_of_essence)
 
-    print('-----------------------------')
 
-    pr_kwargs = {
-        'type_fild': 'type',
-        'type_of_search': 'project',
-    }
-
-    pr = path_handler()
-    pr_view = view()
-    dict_of_projects = pr.dir_travel(pr.fild_search, pr_kwargs)
-    pr_view.sorter(dict_of_projects)
-
-    print('-----------------------------')
-
+def lst(arg):
     types_kwargs = {
-        'type_fild': 'type',
+        'type_fild': args.lst,
     }
 
     types = path_handler()
     types_view = view()
     dict_of_types = types.dir_travel(types.values_search, types_kwargs)
     types_view.sorter_keys(dict_of_types)
+
+
+if __name__ == '__main__':
+    if args.get != '':
+        get(args.get)
+    elif args.lst != '':
+        lst(args.lst)
